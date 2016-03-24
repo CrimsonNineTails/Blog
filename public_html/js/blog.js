b641e09ec7258c5b62b09bb0787c6c300ff73a83
Yo1357 <anders.museth@gmail.com>
2016-03-24 10:58:12 -0700

    video 9 added registration

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+$(function () {
+   var APPLICATION_ID =  "CC3979E6-F655-FD96-FF13-1FAAD8E3DE00",
+       SECRET_KEY = "76B7A6FF-ACD6-07C5-FFF8-BB69AB961B00",
+       VERSION = "v1";
+       
+   Backendless.initApp(APPLICATION_ID,SECRET_KEY, VERSION);
+   
+   var user = new Backendless.user();
+   user.email = "anders.museth@gmail.com"
+   user.password = "password";
+   Backendless.userService.register(sure);
+   
+});
